1ed5dcbb6b1814d46355f2b5885b8f306d841b1b
Sathvik Janga <66199493+27Sathvik@users.noreply.github.com>
2022-08-13 11:49:16 +0530

    Update main.py

--- project-1-hangman/main.py
+++ project-1-hangman/main.py
@@ -1,36 +1,37 @@
-import os
 import random
 
-def hangman():
-  words_file = open("words.txt", "r").read()
-  words = words_file.split('\n')
-  word = random.choice(words)
-  alphabets = ["_"]
-  lives = ['❤️','❤️','❤️','❤️','❤️','❤️','❤️','❤️','❤️','❤️','❤️','❤️']
-  letters = list()
-  while "_" in alphabets or '❤️' not in lives:
-      print(' '.join(lives))
-      print(' '.join(alphabets))
-      alphabets = []
 
-      letter = input("Enter an alphabet: ")
-      while len(letter) != 1 or letter.isdigit() or letter in letters:
-          letter = input("Try again.Enter an alphabet: ")
+words_file = open("words.txt", "r").read()
+words = words_file.split('\n')
+word = random.choice(words)
+alphabets = ["_"]
+lives = ['❤️','❤️','❤️','❤️','❤️','❤️']
+letters = ['a','e','i','o','u']
+while "_" in alphabets or '❤️' not in lives:
+    print(' '.join(lives))
+    print(' '.join(alphabets))
+    alphabets = []
 
-      letters.append(letter)
+    letter = input("Enter an alphabet: ")
+    while len(letter) != 1 or letter.isdigit() or letter in letters:
+        letter = input("Try again.Enter an alphabet: ")
 
-      for alphabet in word:
-          if alphabet in letters:
-              alphabets.append(alphabet)
-          else:
-              alphabets.append("_")
-              
-      if letter not in alphabets:
+    letters.append(letter)
+
+    for alphabet in word:
+        if alphabet in letters:
+            alphabets.append(alphabet)
+        else:
+            alphabets.append("_")
+            
+    if letter not in alphabets:
         lives.pop()
-      os.system('clear')
 
-  print(word)
-  print("You did it you won!!!")
+    if len(lives) == 0:
+        print("you lost")
+        break 
+
+print(word)
 
 if __name__ == "__main__":
   hangman()
